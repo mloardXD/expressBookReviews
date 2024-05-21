fb85c3986fb1546136ed665ad5bd85593b3dff07
mloardXD <mloard2011@hotmail.com>
2024-05-21 15:52:00 -0400

    added author search

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,7 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-
+let bookList = JSON.parse(JSON.stringify(books, null, 4))
 
 public_users.post("/register", (req,res) => {
   //Write your code here
@@ -13,21 +13,29 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  let b = JSON.parse(JSON.stringify(books, null, 4))
 
-  return res.status(300).json({books: b});
+  return res.status(300).json({books: bookList});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //Write your code here  
+
+  return res.status(300).json(bookList[req.params.isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  let a = req.params.author
+  
+  for (const [key, value] of Object.entries(bookList)) {
+    if (value["author"] == a){
+        return res.status(300).json(bookList[key]);
+    }
+  }
+  return res.status(300).json({message: "There is no book with that Author"});
 });
 
 // Get all books based on title
